3e314d4fcf4c6963bc3a517646608f1c70a0cf73
minhdtb <minhdtb.83@gmail.com>
2016-07-27 18:11:29 +0700

    update coding

--- src/main/java/com/minhdtb/storm/StormMethodVisitor.java
+++ src/main/java/com/minhdtb/storm/StormMethodVisitor.java
@@ -5,6 +5,8 @@ import org.objectweb.asm.MethodVisitor;
 import org.objectweb.asm.Opcodes;
 import org.objectweb.asm.commons.AdviceAdapter;
 
+import java.util.ResourceBundle;
+
 class StormMethodVisitor extends AdviceAdapter {
 
     StormMethodVisitor(int api, String owner, int access, String name, String desc, MethodVisitor mv) {
@@ -17,10 +19,30 @@ class StormMethodVisitor extends AdviceAdapter {
 
     @Override
     protected void onMethodEnter() {
+
         Label l0 = new Label();
         mv.visitLabel(l0);
+        mv.visitLineNumber(46, l0);
+        mv.visitVarInsn(ALOAD, 1);
+        Label l1 = new Label();
+        mv.visitJumpInsn(IFNULL, l1);
+        Label l2 = new Label();
+        mv.visitLabel(l2);
+        mv.visitLineNumber(47, l2);
         mv.visitFieldInsn(GETSTATIC, "java/lang/System", "out", "Ljava/io/PrintStream;");
-        mv.visitVarInsn(ILOAD, 0); //1 instead of 0 if PrintInt wasn't static
-        mv.visitMethodInsn(INVOKEVIRTUAL, "java/io/PrintStream", "println", "(I)V", false);
+        mv.visitVarInsn(ALOAD, 1);
+        mv.visitMethodInsn(INVOKEVIRTUAL, "java/io/PrintStream", "println", "(Ljava/lang/String;)V", false);
+        mv.visitLabel(l1);
+        mv.visitLineNumber(49, l1);
+        mv.visitFrame(Opcodes.F_SAME, 0, null, 0, null);
+
+        mv.visitEnd();
+
+    }
+
+    private void testXXX(String value) {
+        if (value != null) {
+            System.out.println(value);
+        }
     }
 }
